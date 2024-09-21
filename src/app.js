9efd9ce89c748e92a060529099c802beb4b3b4cf
nino <pnekoneko907@gmail.com>
2024-09-21 16:02:22 +0900

    add express sample api

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log("Hello World!!!!!!!!");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const server = app.listen(3000, function () {
+  console.log("NOde.js is listening to PORT:" + server.address().port);
+});
+
+app.disable("x-powerd-by");
+app.use(cors()).use(express.json());
+
+app.get("/", (req, res) => {
+  // res.send("hello world!!");
+  res.json({
+    id: "1",
+    name: "hogeohge",
+    description: "hello world",
+  });
+});
